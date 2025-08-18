88ff291e36e56538608ccd488fae715c5141881a
Gabriel Bueno <Gabrielbc147@gmail.com>
2025-08-18 20:06:48 -0300

    Exercicios Python para casa

---
+++ exercicios_casa/1_3.py
@@ -0,0 +1,18 @@
+# Faça um programa que receba o raio de uma circunferência em centímetros. 
+# Retorne para o usuário qual é a área e perímetro desta circunferência no seguinte formato.
+# Área:  x.xx
+# Perímetro:  y.yy
+
+def get_input():
+        while True:
+            try:
+                raio = float(input("Entre com o raio da circunferência: "))
+            except ValueError as err:
+                print("Valor inválido!")
+                continue
+            
+            
+
+       
+        
+
