140d9e7914b5d390732cb5cbb23cd90861d7d115
Gabriel Bueno <Gabrielbc147@gmail.com>
2025-08-19 21:30:11 -0300

    Mais exercicios python

--- exercicios_casa/1_3.py
+++ exercicios_casa/1_3.py
@@ -2,17 +2,29 @@
 # Retorne para o usuário qual é a área e perímetro desta circunferência no seguinte formato.
 # Área:  x.xx
 # Perímetro:  y.yy
+import math as mh
 
 def get_input():
         while True:
             try:
                 raio = float(input("Entre com o raio da circunferência: "))
+                if raio <= 0:
+                     print("O raio deve ser um número positivo!")
+                     continue
+                return raio # Return entrega o resultado de uma função para quem a chamou (Pense na função como uma cozinha: você faz um pedido (chama a função), ela prepara o prato (executa o código), e o return é o garçom trazendo o prato até você.Sem return, a função faz tudo lá dentro, mas não te entrega nada.)
             except ValueError as err:
                 print("Valor inválido!")
                 continue
             
-            
+raio = get_input()
 
+area = mh.pi * (raio ** 2)
+perimetro = 2 * mh.pi * raio
+
+area = round(area, 2)
+perimetro = round(perimetro,2)
+
+print("A area dessa circunferencia é:", area, "e o preimetro é:", perimetro)
        
         
 
